5c3f9481493879c643ac44848635293a313a41f5
jhosyta13 <117184775+j23591492@users.noreply.github.com>
2022-11-19 22:19:16 -0300

    Add files via upload

---
+++ javascrpt4/temperatura.js
@@ -0,0 +1,11 @@
+var temperatura =prompt("temperatura");
+temperatura =parseFloat(temperatura);
+if (temperatura>38){
+    document.write("esta con febre");
+    console.log("esta con febre");
+
+}
+else{
+    document.write("sem febre");
+    console.log("sem febre");
+}
